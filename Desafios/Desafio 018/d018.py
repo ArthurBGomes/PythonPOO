6f1ea74cf49945f0c88411aaa432a180dc52a9b0
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2026-03-03 17:15:37 -0300

    Ajeitando desafio 18

--- Desafios/Desafio 018/d018.py
+++ Desafios/Desafio 018/d018.py
@@ -1,16 +1,21 @@
+from rich import print
+from rich.panel import Panel
 class Churrasco:
-    def __init__(self, titulo, pessoas, consumo_por_pessoa=0.4, preco_kg=82.40):
+    consumo_por_pessoa=0.4
+    preco_kg=82.40
+    def __init__(self, titulo, pessoas,):
         self.titulo = titulo
         self.pessoas = pessoas
-        self.consumo_por_pessoa = consumo_por_pessoa
-        self.preco_kg = preco_kg
-    def analisar(self,totalcusto=0,preco = 0):
-        print(f"Analisando {self.titulo} com {self.pessoas} convidados")
-        print(f"Cada participante comerá 0.4kg e cada kg custa R$82.40")
-        print(f"Recomendo comprar {(self.pessoas *self.consumo_por_pessoa):.3f}KG")
-        custo = (self.pessoas * self.consumo_por_pessoa) * 82.40
-        print(f"o Custo total será de R${custo:.2f}")
+
+    def analisar(self,):
+        conteudo = f"Analisando [green]{self.titulo}[/] com [blue]{self.pessoas} convidados[/]"
+        conteudo +=f"\nCada participante comerá 0.4kg e cada kg custa R$82.40"
+        conteudo += f"\nRecomendo comprar {(self.pessoas *Churrasco.consumo_por_pessoa):.3f}KG"
+        custo = (self.pessoas * __class__.consumo_por_pessoa) * 82.40
+        conteudo +=f"\nO Custo total será de R${custo:.2f}"
         preco = custo/self.pessoas
-        print(f"cada pessoa pagará R${preco:.2f} para participar")
+        conteudo += f"\nCada pessoa pagará R${preco:.2f} para participar"
+        panel = Panel(conteudo,title = self.titulo)
+        print(panel)
 c1 = Churrasco("Churras dos Amigos",15)
 c1.analisar()
