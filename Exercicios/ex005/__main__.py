b0646a8761ff4124385ab545a87f3af7aac3ac1c
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2026-04-07 22:55:27 -0300

    07/04/2026
    -Aula 07- Herança -Curso em Vídeo

---
+++ Exercicios/ex005/__main__.py
@@ -0,0 +1,17 @@
+from rich import inspect
+from classesex005 import Aluno,Professor,Funcionario
+def main():
+    a1 = Aluno("Arthur",16,"Informática","INFO1V")
+    a1.fazer_aniversario()
+    a1.fazer_matricula()
+    inspect(a1,methods=True)
+    p1 = Professor("Léo",38,"Eng.Software","Doutor")
+    p1.fazer_aniversario()
+    p1.dar_aula()
+    inspect(p1)
+    f1 = Funcionario("George",16,"Porteiro","Portaria")
+    f1.fazer_aniversario()
+    f1.bater_ponto()
+    inspect(f1)
+if __name__ == "__main__":
+    main()
